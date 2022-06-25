e93d1d01af056c6fb06aa7ffcc9d9a6002a5632a
walkerxin <walkerxinH@gmail.com>
2022-06-26 00:28:24 +0800

    💄  import bootstrap css style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  username = 'my-app';
+
+  onGoToClearUsername(): void {
+    this.username = '';
+  }
 }
