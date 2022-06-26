e0372edea0d382696853103a378d1b76329d6cdd
walkerxin <walkerxinH@gmail.com>
2022-06-26 13:41:08 +0800

    practice build-in directive(structual & attribute)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username = 'my-app';
+  isFree = false;
+  logArr: Date[] = []
 
   onGoToClearUsername(): void {
     this.username = '';
+  }
+
+  genLog(): void {
+    this.logArr.push(new Date());
+  }
+
+  getLogStyle(i: number): Object {
+    return i >= 5 ? { 'background-color': 'blue' } : Object.create(null);
   }
 }
