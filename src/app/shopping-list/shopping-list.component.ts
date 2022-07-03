d39e2f64ec2d4ef9c0b800002de0bf1a74dfc64d
walkerxin <walkerxinH@gmail.com>
2022-07-03 21:51:23 +0800

    feat: all components

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-shopping-list',
+    template: '<div>a shopping list</div>'
+})
+export class ShoppingListComponent {
+
+}
